68792eec634bf3da550e4637716984ae2be381a8
mykha <mykha@10.0.0.123>
2021-05-10 11:03:31 -0500

    added exercise #1

---
+++ src/Module1/exercise_01.java
@@ -0,0 +1,25 @@
+package Module1;
+/*
+Challenge:
+
+Given a two-dimensional array of integers, return the flattened version of the array 
+with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+*/
+
+public class exercise_01 {
+
+	public static int[] flattenAndSort(int[][] array) {
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
