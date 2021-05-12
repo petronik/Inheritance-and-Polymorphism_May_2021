b5316406ab1dcd61d6ce2e7f63a2abef49cbee12
Petronik <petronik@ukr.net>
2021-05-11 20:50:14 -0500

    solving problem arr[][]

--- src/Module1/exercise_01.java
+++ src/Module1/exercise_01.java
@@ -1,4 +1,4 @@
-package Module1;
+
 /*
 Challenge:
 
@@ -10,16 +10,32 @@ Example:
 Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 */
 
+
+import java.util.Arrays;
+
 public class exercise_01 {
-
+	
 	public static int[] flattenAndSort(int[][] array) {
+		int len = 0;
 
-		return null;
+		for (int i = 0; i < array.length; i++) {
+			len += array[i].length;
+		}
+		int[] res = new int[len];
+		int k = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				res[k++] = array[i][j];
+			}
+		}
+		Arrays.sort(res);
+		return res;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(
+			Arrays.toString(flattenAndSort(new int[][] {{3, 2, 1}, {4, 6, 5}, {555, 465}, {9, 8, 7}}))
+			);
 	}
 
 }
